48da5ea04194c1d083ecaf31f8d135294496c2e3
Hugo Silva <yhugorocha@gmail.com>
2024-07-12 08:33:41 -0300

    FEAT: Add initial commit

---
+++ src/main/java/com/hugodev/magalums/MagalumsApplication.java
@@ -0,0 +1,13 @@
+package com.hugodev.magalums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagalumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagalumsApplication.class, args);
+	}
+
+}
