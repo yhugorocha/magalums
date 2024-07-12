88edee7854b5eb11c36f773250f20af90f09e030
Hugo Silva <yhugorocha@gmail.com>
2024-07-12 18:51:17 -0300

    FEAT: Add notification repository.

--- src/main/java/com/hugodev/magalums/repository/NotificationRepository.java
+++ src/main/java/com/hugodev/magalums/repository/NotificationRepository.java
@@ -1,7 +1,12 @@
 package com.hugodev.magalums.repository;
 
 import com.hugodev.magalums.entity.Notification;
+import com.hugodev.magalums.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByStatusInAndDateTimeBefore(List<Status> status, LocalDate date);
 }
