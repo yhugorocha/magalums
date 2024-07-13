84988198b2803eb1e1c697e1d89a75aec4e7b74c
Hugo Silva <yhugorocha@gmail.com>
2024-07-13 10:59:59 -0300

    FEAT: Add WHATSAPP sending service with Twilio.

--- src/main/java/com/hugodev/magalums/service/NotificationService.java
+++ src/main/java/com/hugodev/magalums/service/NotificationService.java
@@ -5,6 +5,8 @@ import com.hugodev.magalums.dto.NotificationResponse;
 import com.hugodev.magalums.entity.Notification;
 import com.hugodev.magalums.entity.Status;
 import com.hugodev.magalums.repository.NotificationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +18,13 @@ import java.util.function.Consumer;
 @Service
 public class NotificationService {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     NotificationRepository notificationRepository;
 
     @Autowired
-    SMSService smsService;
+    TwilioService twilioService;
 
     public NotificationResponse createNotification(NotificationRequest notification){
        Notification newNotification = this.notificationRepository.save(new Notification(notification));
@@ -48,22 +52,30 @@ public class NotificationService {
     private Consumer<Notification> sendNotification(){
 
         return n -> {
-            switch (n.getChannel().getDescription()){
-                case "sms":
-                    this.smsService.sendSMS(n.getDestination(), n.getMessage());
-                    break;
-                case "email":
-                    //TODO Create email send
-                    break;
-                case "whatsapp":
-                    //TODO Create whatsapp send
-                    break;
-                case "push":
-                    //TODO Create push send
-                    break;
+            try {
+                switch (n.getChannel().getDescription()) {
+                    case "sms":
+                        this.twilioService.sendSMS(n.getDestination(), n.getMessage());
+                        break;
+                    case "email":
+                        //TODO Create email send
+                        break;
+                    case "whatsapp":
+                        this.twilioService.sendWPP(n.getDestination(), n.getMessage());
+                        break;
+                    case "push":
+                        //TODO Create push send
+                        break;
+                }
+
+                n.setStatus(Status.Values.SUCESS.toStatus());
+                this.notificationRepository.save(n);
+
+            }catch (Exception e){
+                LOGGER.info("ERROR: "+ e.getMessage());
+                n.setStatus(Status.Values.ERROR.toStatus());
+                this.notificationRepository.save(n);
             }
-            n.setStatus(Status.Values.SUCESS.toStatus());
-            this.notificationRepository.save(n);
         };
     }
 }
--- src/main/java/com/hugodev/magalums/service/TwilioService.java
+++ src/main/java/com/hugodev/magalums/service/TwilioService.java
@@ -7,22 +7,33 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SMSService {
+public class TwilioService {
     private final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
     private final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
     private final String NUMBER_SENDER = System.getenv("TWILIO_NUMBER_SENDER");
+    private final String WPP_NUMBER_SENDER = System.getenv("TWILIO_WPP_NUMBER_SENDER");
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     public void sendSMS(String recipient, String content){
+        sendToTwilio(recipient, content, NUMBER_SENDER);
+    }
+
+    public void sendWPP(String recipient, String content){
+        sendToTwilio("whatsapp:"+recipient, content, WPP_NUMBER_SENDER);
+    }
+
+    private void sendToTwilio(String recipient, String content, String numberSender){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                         new com.twilio.type.PhoneNumber(recipient),
-                        new com.twilio.type.PhoneNumber(NUMBER_SENDER),
+                        new com.twilio.type.PhoneNumber(numberSender),
                         content)
                 .create();
 
-
         LOGGER.info("Message sent to: "+ recipient +" content: "+message.getBody());
+        LOGGER.info("Status: "+message.getStatus());
+        LOGGER.info("Erro Message: "+message.getErrorMessage());
+        LOGGER.info("Get Sid: "+message.getSid());
     }
 }
